66cc69fb63191c406f5818dccc12454916d3a7e2
peterupton <peter@peterupton.xyz>
2020-10-24 20:50:39 -0500

    remove JSON support, remove separator styles

--- README.md
+++ README.md
@@ -1,36 +1,16 @@
 flatten
 =======
 
-[![GoDoc](https://godoc.org/github.com/jeremywohl/flatten?status.png)](https://godoc.org/github.com/jeremywohl/flatten)
-[![Build Status](https://travis-ci.org/jeremywohl/flatten.svg?branch=master)](https://travis-ci.org/jeremywohl/flatten)
+[![GoDoc](https://godoc.org/github.com/peterupton/flatten?status.png)](https://godoc.org/github.com/peterupton/flatten)
+[![Build Status](https://travis-ci.org/peterupton/flatten.svg?branch=master)](https://travis-ci.org/peterupton/flatten)
 
 Flatten makes flat, one-dimensional maps from arbitrarily nested ones.
 
-It turns map keys into compound
-names, in four default styles: dotted (`a.b.1.c`), path-like (`a/b/1/c`), Rails (`a[b][1][c]`), or with underscores (`a_b_1_c`).  Alternatively, you can pass a custom style.
+It turns map keys into compound names.
 
-It takes input as either JSON strings or
-Go structures.  It knows how to traverse these JSON types: objects/maps, arrays and scalars.
+It takes input as Go structures.
 
-You can flatten JSON strings.
 
-```go
-nested := `{
-  "one": {
-    "two": [
-      "2a",
-      "2b"
-    ]
-  },
-  "side": "value"
-}`
-
-flat, err := flatten.FlattenString(nested, "", flatten.DotStyle)
-
-// output: `{ "one.two.0": "2a", "one.two.1": "2b", "side": "value" }`
-```
-
-Or Go maps directly.
 
 ```go
 nested := map[string]interface{}{
@@ -42,24 +22,16 @@ nested := map[string]interface{}{
    "z": 1.4567,
 }
 
-flat, err := flatten.Flatten(nested, "", flatten.RailsStyle)
+flat, err := flatten.Flatten(nested, ".")
 
 // output:
 // map[string]interface{}{
 //  "a":    "b",
-//  "c[d]": "e",
-//  "c[f]": "g",
+//  "c.d": "e",
+//  "c.f": "g",
 //  "z":    1.4567,
 // }
 ```
 
-Let's try a custom style, with the first example above.
 
-```go
-emdash := flatten.SeparatorStyle{Middle: "--"}
-flat, err := flatten.FlattenString(nested, "", emdash)
-
-// output: `{ "one--two--0": "2a", "one--two--1": "2b", "side": "value" }`
-```
-
-See [godoc](https://godoc.org/github.com/jeremywohl/flatten) for API.
+See [godoc](https://godoc.org/github.com/peterupton/flatten) for API.
--- flatten_test.go
+++ flatten_test.go
@@ -3,17 +3,14 @@ package flatten
 import (
 	"encoding/json"
 	"reflect"
-	"strings"
 	"testing"
-	"unicode"
 )
 
 func TestFlatten(t *testing.T) {
 	cases := []struct {
-		test   string
-		want   map[string]interface{}
-		prefix string
-		style  SeparatorStyle
+		test      string
+		want      map[string]interface{}
+		separator string
 	}{
 		{
 			`{
@@ -46,8 +43,7 @@ func TestFlatten(t *testing.T) {
 				"number":       1.4567,
 				"bool":         true,
 			},
-			"",
-			DotStyle,
+			".",
 		},
 		{
 			`{
@@ -68,16 +64,15 @@ func TestFlatten(t *testing.T) {
 				}
 			}`,
 			map[string]interface{}{
-				"foo[jim]":        "bean",
-				"fee":             "bar",
-				"n1[alist][0]":    "a",
-				"n1[alist][1]":    "b",
-				"n1[alist][2]":    "c",
-				"n1[alist][3][d]": "other",
-				"n1[alist][3][e]": "another",
+				"foo-jim":      "bean",
+				"fee":          "bar",
+				"n1-alist-0":   "a",
+				"n1-alist-1":   "b",
+				"n1-alist-2":   "c",
+				"n1-alist-3-d": "other",
+				"n1-alist-3-e": "another",
 			},
-			"",
-			RailsStyle,
+			"-",
 		},
 		{
 			`{
@@ -110,17 +105,7 @@ func TestFlatten(t *testing.T) {
 				"number":       1.4567,
 				"bool":         true,
 			},
-			"",
-			PathStyle,
-		},
-		{
-			`{ "a": { "b": "c" }, "e": "f" }`,
-			map[string]interface{}{
-				"p:a.b": "c",
-				"p:e":   "f",
-			},
-			"p:",
-			DotStyle,
+			"/",
 		},
 		{
 			`{
@@ -153,8 +138,7 @@ func TestFlatten(t *testing.T) {
 				"number":       1.4567,
 				"bool":         true,
 			},
-			"",
-			UnderscoreStyle,
+			"_",
 		},
 	}
 
@@ -165,147 +149,8 @@ func TestFlatten(t *testing.T) {
 			t.Errorf("%d: failed to unmarshal test: %v", i+1, err)
 			continue
 		}
-		got, err := Flatten(m.(map[string]interface{}), test.prefix, test.style)
-		if err != nil {
-			t.Errorf("%d: failed to flatten: %v", i+1, err)
-			continue
-		}
+		got, _ := Flatten(m.(map[string]interface{}), test.separator)
 		if !reflect.DeepEqual(got, test.want) {
-			t.Errorf("%d: mismatch, got: %v wanted: %v", i+1, got, test.want)
-		}
-	}
-}
-
-func TestFlattenString(t *testing.T) {
-	cases := []struct {
-		test   string
-		want   string
-		prefix string
-		style  SeparatorStyle
-		err    error
-	}{
-		// 1
-		{
-			`{ "a": "b" }`,
-			`{ "a": "b" }`,
-			"",
-			DotStyle,
-			nil,
-		},
-		// 2
-		{
-			`{ "a": { "b" : { "c" : { "d" : "e" } } }, "number": 1.4567, "bool": true }`,
-			`{ "a.b.c.d": "e", "bool": true, "number": 1.4567 }`,
-			"",
-			DotStyle,
-			nil,
-		},
-		// 3
-		{
-			`{ "a": { "b" : { "c" : { "d" : "e" } } }, "number": 1.4567, "bool": true }`,
-			`{ "a/b/c/d": "e", "bool": true, "number": 1.4567 }`,
-			"",
-			PathStyle,
-			nil,
-		},
-		// 4
-		{
-			`{ "a": { "b" : { "c" : { "d" : "e" } } } }`,
-			`{ "a--b--c--d": "e" }`,
-			"",
-			SeparatorStyle{Middle: "--"}, // emdash
-			nil,
-		},
-		// 5
-		{
-			`{ "a": { "b" : { "c" : { "d" : "e" } } } }`,
-			`{ "a(b)(c)(d)": "e" }`,
-			"",
-			SeparatorStyle{Before: "(", After: ")"}, // paren groupings
-			nil,
-		},
-		// 6 -- with leading whitespace
-		{
-			`
-			  	{ "a": { "b" : { "c" : { "d" : "e" } } } }`,
-			`{ "a(b)(c)(d)": "e" }`,
-			"",
-			SeparatorStyle{Before: "(", After: ")"}, // paren groupings
-			nil,
-		},
-
-		//
-		// Valid JSON text, but invalid for FlattenString
-		//
-
-		// 7
-		{
-			`[ "a": { "b": "c" }, "d" ]`,
-			`bogus`,
-			"",
-			PathStyle,
-			NotValidJsonInputError,
-		},
-		// 8
-		{
-			``,
-			`bogus`,
-			"",
-			PathStyle,
-			NotValidJsonInputError,
-		},
-		// 9
-		{
-			`astring`,
-			`bogus`,
-			"",
-			PathStyle,
-			NotValidJsonInputError,
-		},
-		// 10
-		{
-			`false`,
-			`bogus`,
-			"",
-			PathStyle,
-			NotValidJsonInputError,
-		},
-		// 11
-		{
-			`42`,
-			`bogus`,
-			"",
-			PathStyle,
-			NotValidJsonInputError,
-		},
-		// 12 -- prior to version 1.0.1, this was accepted & unmarshalled as an empty map, finally returning `{}`.
-		{
-			`null`,
-			`{}`,
-			"",
-			PathStyle,
-			NotValidJsonInputError,
-		},
-	}
-
-	for i, test := range cases {
-		got, err := FlattenString(test.test, test.prefix, test.style)
-		if err != test.err {
-			t.Errorf("%d: error mismatch, got: [%v], wanted: [%v]", i+1, err, test.err)
-			continue
-		}
-		if err != nil {
-			continue
-		}
-
-		nixws := func(r rune) rune {
-			if unicode.IsSpace(r) {
-				return -1
-			}
-			return r
-		}
-
-		if got != strings.Map(nixws, test.want) {
 			t.Errorf("%d: mismatch, got: %v wanted: %v", i+1, got, test.want)
 		}
 	}
